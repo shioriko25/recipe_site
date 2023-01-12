472e954bd0a3fec67bb686c32a0edc0b402287f4
shioriko25 <m.kmkmkaassfh.12@gmail.com>
2023-01-12 14:37:25 +0000

    [add]会員実装

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,6 +10,14 @@ class Customer < ApplicationRecord
     
     has_one_attached :image
     
-    
+      
+  def get_image #画像が存在しない場合に表示する画像をActiveStorageに格納する
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+
     
 end
